e2271b4d86d769a540f224ea4a07c36585f1551b
ricoslick <alias.co>
2019-06-11 04:51:22 -0700

    Settings decoupling

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,6 +157,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/dynamics/'
 LOGOUT_REDIRECT_URL = '/dynamics/'
