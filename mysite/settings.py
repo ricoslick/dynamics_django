be3097ce7e9a2ce30c1c3252d6481bb0c8467f1d
Eric Wasike <ewj@EAPI.local>
2023-02-14 19:33:26 +0300

    comment out RENDER_EXTERNAL_HOSTNAME variable

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,11 +50,9 @@ DATABASES = {
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.herokuapp.com', '.onrender.com']
 
 # RENDER_EXTERNAL_HOSTNAME Variable
-RENDER_EXTERNAL_HOSTNAME = 'https://dynamics-django.onrender.com'
+# RENDER_EXTERNAL_HOSTNAME = 'https://dynamics-django.onrender.com'
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:    
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # File logging
 LOGGING = {
